76d6293fb71aba6f39e4c84c71faf52921ea44d9
milosz.kucharski <miloszkucharski@gmail.com>
2024-10-25 23:49:25 +0200

    Refactor Redux store and weather slices

--- src/app/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import weatherSlice from '../features/weather/weatherSlices';
-
-export const store = configureStore({
-  reducer: {
-    weather: weatherSlice,
-  },
-});
