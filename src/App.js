2a269c57a08b7ac9f2d38307aca3206e048305f6
milosz.kucharski <miloszkucharski@gmail.com>
2024-10-25 23:49:13 +0200

    Refactor App component to include LeftBar component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import {Weather} from './features/weather/Weather';
 import './App.css';
+import {LeftBar} from './features/leftBar/LeftBar';
 
 function App() {
-  return (
-    <div className="App">
-      <Weather />
-    </div>
-  );
+    return (
+        <div className="App">
+            <LeftBar />
+            <Weather />
+        </div>
+    );
 }
 
 export default App;
